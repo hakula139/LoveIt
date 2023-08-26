f65bdffffe6970c20838bf909da488ebf21604f5
Hakula Chen <i@hakula.xyz>
2023-08-26 14:05:48 +0800

    feat(comment): support lightgallery for images in comments

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
@@ -457,7 +463,8 @@ var Theme = /*#__PURE__*/function () {
   }, {
     key: "initLightGallery",
     value: function initLightGallery() {
-      if (this.config.lightgallery) lightGallery(document.getElementById('content'), {
+      var el = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : document.getElementById('content');
+      if (this.config.lightgallery) lightGallery(el, {
         plugins: [lgThumbnail, lgZoom],
         selector: '.lightgallery',
         speed: 400,
@@ -841,12 +848,31 @@ var Theme = /*#__PURE__*/function () {
           this.config.comment.gitalk.body = decodeURI(window.location.href);
           var gitalk = new Gitalk(this.config.comment.gitalk);
           gitalk.render('gitalk');
-        }
+        } else if (this.config.comment.valine) {
+          new Valine(this.config.comment.valine);
+        } else if (this.config.comment.twikoo) {
+          twikoo.init(_objectSpread(_objectSpread({}, this.config.comment.twikoo), {}, {
+            onCommentLoaded: function onCommentLoaded() {
+              _this11.util.forEach(document.getElementsByClassName('tk-content'), function ($content) {
+                var $imgElements = $content.querySelectorAll(':not(.lightgallery) > img:not(.tk-owo-emotion)');
 
-        if (this.config.comment.valine) new Valine(this.config.comment.valine);
-        if (this.config.comment.twikoo) twikoo.init(this.config.comment.twikoo);
+                if ($imgElements.length > 0) {
+                  _this11.util.forEach($imgElements, function ($img) {
+                    var $wrapper = document.createElement('a');
+                    $wrapper.setAttribute('class', 'lightgallery');
+                    $wrapper.setAttribute('href', $img.getAttribute('src'));
+                    $wrapper.setAttribute('title', $img.getAttribute('alt'));
+                    $wrapper.setAttribute('data-thumbnail', $img.getAttribute('src'));
+                    $img.parentNode.insertBefore($wrapper, $img);
+                    $wrapper.appendChild($img);
+                  });
 
-        if (this.config.comment.utterances) {
+                  _this11.initLightGallery($content);
+                }
+              });
+            }
+          }));
+        } else if (this.config.comment.utterances) {
           var utterancesConfig = this.config.comment.utterances;
           var script = document.createElement('script');
           script.src = 'https://utteranc.es/client.js';
@@ -869,9 +895,7 @@ var Theme = /*#__PURE__*/function () {
           };
 
           this.switchThemeEventSet.add(this._utterancesOnSwitchTheme);
-        }
-
-        if (this.config.comment.giscus) {
+        } else if (this.config.comment.giscus) {
           var giscusConfig = this.config.comment.giscus;
           var giscusScript = document.createElement('script');
           giscusScript.src = 'https://giscus.app/client.js';
